231a9e41b1ff19ad1223fd823697fa68595ab347
Caleb Courier <caleb.courier@gmail.com>
2023-08-02 10:18:38 -0500

    update merge for yq 4.x

--- merge.sh
+++ merge.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-merged=$(yq ea '. as $item ireduce ({}; . * $item )' ./service-specs/*.yml)
-yq -P <<< $merged > open-api-spec.yml
-consolidated=$(yq eval-all 'select(fileIndex==0).info = select(fileIndex==1).info | select(fileIndex==0)'  ./open-api-spec.yml ./service-specs/guardrails-service-spec.yml)
-yq -P <<< $consolidated > open-api-spec.yml
+echo "merging files"
+yq eval-all '. as $item ireduce ({}; . * $item )' ./service-specs/*.yml > open-api-spec.yml
+
+echo "consolidating info"
+yq eval-all --inplace 'select(fileIndex==0).info = select(fileIndex==1).info | select(fileIndex==0)' ./open-api-spec.yml ./service-specs/guardrails-service-spec.yml
