ae0aae31fd062b02ebab08672f456481a12c1068
Caleb Courier <caleb.courier@gmail.com>
2023-08-10 08:49:44 -0500

    use module pattern for pip install

--- build-sdk.sh
+++ build-sdk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install openapi-python-client;
+python3 -m pip install openapi-python-client;
 version=$(yq '.info.version' open-api-spec.yml);
 rm -rf ./guard-rails-api-client;
 openapi-python-client generate --path ./open-api-spec.yml --meta setup;
