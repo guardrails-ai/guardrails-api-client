9b78e8f6a6b981fe7032727e9c2715b82660fd8d
Caleb Courier <caleb.courier@gmail.com>
2024-03-08 10:49:45 -0600

    add config to control version in generated package

--- build-sdk.sh
+++ build-sdk.sh
@@ -2,4 +2,4 @@
 python3 -m pip install openapi-python-client;
 version=$(yq '.info.version' open-api-spec.yml);
 rm -rf ./guard-rails-api-client;
-openapi-python-client generate --path ./open-api-spec.yml --meta setup;
+openapi-python-client generate --config ./config.json --path ./open-api-spec.yml --meta setup;
