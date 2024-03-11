67e422472bd4a1ac46af30ff79203df6b8bb1108
CalebCourier <CalebCourier@users.noreply.github.com>
2024-03-11 20:40:46 +0000

    update generated sdk

--- guardrails-api-client/setup.py
+++ guardrails-api-client/setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="guardrails-api-client",
-    version="0.0.2",
+    version="0.1.0",
     description="A client library for accessing Guardrails API",
     long_description=long_description,
     long_description_content_type="text/markdown",
