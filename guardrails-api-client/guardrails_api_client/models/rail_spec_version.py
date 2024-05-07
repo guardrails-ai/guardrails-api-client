3d91152aa134885c0d62bae724adf7bee61b9640
Caleb Courier <caleb.courier@gmail.com>
2024-05-07 11:52:30 -0500

    deprecate other services, delete auto-gen code

--- guardrails-api-client/guardrails_api_client/models/rail_spec_version.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class RailSpecVersion(str, Enum):
-    VALUE_0 = "0.1"
-
-    def __str__(self) -> str:
-        return str(self.value)
--- guardrails-api-client/guardrails_api_client/models/status.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class Status(str, Enum):
-    FAIL = "fail"
-    PASS = "pass"
-
-    def __str__(self) -> str:
-        return str(self.value)
